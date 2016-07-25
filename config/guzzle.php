16e68eb14a2954b581e48e9f86323d0ce117aed7
lovenery <edittye379@gmail.com>
2016-07-25 09:12:19 +0800

    可以跑的狀態

---
+++ config/guzzle.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'portal' => [
+        'client_id' => env('PORTAL_CLIENT_ID'),
+        'client_secret' => env('PORTAL_CLIENT_SECRET'),
+        'callback' => env('PORTAL_CALLBACK')
+    ]
+];
